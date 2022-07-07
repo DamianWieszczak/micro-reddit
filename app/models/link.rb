4cc4863e41ff15e664b9611296296873278cca9b
DamianWieszczak <damian.wieszczak@gmail.com>
2022-07-08 00:04:25 +0200

    Update validations

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,7 @@ class Link < ApplicationRecord
   belongs_to :user
   acts_as_votable
   has_many :comments
+  validates :title, presence: true, length: { minimum: 3, maximum: 80 }
+  validates :url, presence: true, length: { minimum: 3, maximum: 200 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :links
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 40 }
+
 end
